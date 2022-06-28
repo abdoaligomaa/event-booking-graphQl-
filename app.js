61198255756844acaad23b66c26d005ee0b58ec6
abdoaligomaa <abdoaligomaa107@gmail.com>
2022-06-28 19:00:44 +0200

    adding demo querey and mutations

--- app.js
+++ app.js
@@ -2,25 +2,43 @@ const express=require('express')
 const app=express()
 const port=process.env.PORT || 3000
 
+var bodyParser = require("body-parser");
+
+
+
+// parse application/x-www-form-urlencoded
+// app.use(bodyParser.urlencoded({ extended: false }));
+
+// parse application/json
+app.use(bodyParser.json());
+
 const {graphqlHTTP}=require('express-graphql')
 const {buildSchema}=require('graphql')
 
+// app.use(express.json())
 
-app.use(
+app.use("/graphql",
   graphqlHTTP({
     schema: buildSchema(`
         type RootQuery{
-            sayWelcome:String
+            sayWelcome:String!
         }
         type RootMutation{
-            sayHellow(name:String):String
+            sayHellow(name:String):String!
         }
         schema{
             query:RootQuery
             mutation:RootMutation
         }
     `),
-    rootValue: {},
+    rootValue: {
+        saywelcome:()=>{
+            return "welcome to graphql projcet"
+        },
+        sayHellow:(name)=>{
+            return `hellow ${name}, how are you !`
+        }
+    },
     graphiql: true,
   })
 );
