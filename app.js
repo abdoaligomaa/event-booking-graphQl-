b7704082be671fb680a8220bf2390acf76eaaa60
abdoaligomaa <abdoaligomaa107@gmail.com>
2022-06-28 18:43:07 +0200

    adding graphqlhttp middlewre

--- app.js
+++ app.js
@@ -1,7 +1,17 @@
 const express=require('express')
-
 const app=express()
 const port=process.env.PORT || 3000
+
+const {graphqlHTTP}=require('express-graphql')
+const {buildSchema}=require('graphql')
+
+
+app.use(graphqlHTTP({
+    schema:null,
+    rootValue:{},
+    graphiql:true
+    
+}))
 
 app.get('/',(req,res)=>{
     res.send('welcome in my first graphql project...............')
